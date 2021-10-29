62d506b76cd6f96c56e3c91b31db12cec1e3c9fb
Fola Flor <dpcalfola@gmail.com>
2021-10-29 21:07:34 +0900

    Fourth commit

---
+++ first_project/src/multiplication/table3.java
@@ -0,0 +1,16 @@
+package multiplication;
+
+public class table3 {
+	public static void main(String[] args) {
+		System.out.println(3 * 1);
+		System.out.println(3 * 2);
+		System.out.println(3 * 3);
+		System.out.println(3 * 4);
+		System.out.println(3 * 5);
+		System.out.println(3 * 6);
+		System.out.println(3 * 7);
+		System.out.println(3 * 8);
+		System.out.println(3 * 9);
+	}
+
+}
---
+++ first_project/src/multiplication/table4.java
@@ -0,0 +1,14 @@
+package multiplication;
+
+public class table4 {
+	public static void main(String[] args) {
+		
+		
+		
+			
+			
+		}
+		
+	}
+
+}
