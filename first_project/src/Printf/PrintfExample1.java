a23051a152699b008e5b233eca9e0ac3605cdf89
Fola Flor <dpcalfola@gmail.com>
2021-12-28 09:13:10 +0900

    feat: .Printf

---
+++ first_project/src/Printf/PrintfExample1.java
@@ -0,0 +1,11 @@
+package Printf;
+
+public class PrintfExample1 {
+    public static void main(String[] args) {
+        System.out.printf("%10s%10s%10s\n", "item", "count","prince" );
+
+        // % -> 기준
+        // s -> 문자열
+
+    }
+}
